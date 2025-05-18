26428b9ba5e2d62fe3d68cd529e052a74a705358
peexthree <linedrop@yandex.ru>
2025-05-18 03:28:35 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+﻿// Инициализация анимаций
+new WOW().init();
+
+// Отправка формы в Telegram
+document.querySelector('.lead-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    fetch(`https://api.telegram.org/bot8104403306:AAEH9qUcQaoryZV7Ws9HB0AEqp_sdOgrbro/sendMessage?chat_id=5178416366&text=Новая заявка`);
+});
