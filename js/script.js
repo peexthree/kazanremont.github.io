8a809f68930ac59d7865bf2bd12630a558a3009c
peexthree <linedrop@yandex.ru>
2025-05-18 03:40:49 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-﻿// Инициализация анимаций
-new WOW().init();
-
-// Отправка формы в Telegram
-document.querySelector('.lead-form').addEventListener('submit', function(e) {
+// Отправка формы в Telegram-бота
+document.getElementById('order-form').addEventListener('submit', function(e) {
     e.preventDefault();
-    fetch(`https://api.telegram.org/bot8104403306:AAEH9qUcQaoryZV7Ws9HB0AEqp_sdOgrbro/sendMessage?chat_id=5178416366&text=Новая заявка`);
+    
+    const name = this.querySelector('input[type="text"]').value;
+    const phone = this.querySelector('input[type="tel"]').value;
+    
+    // Отправка в бота
+    fetch(`https://api.telegram.org/bot8104403306:AAEH9qUcQaoryZV7Ws9HB0AEqp_sdOgrbro/sendMessage?chat_id=5178416366&text=Новая заявка:%0AИмя: ${name}%0AТелефон: ${phone}`);
+    
+    // Очистка формы
+    this.reset();
+    alert('Заявка отправлена! Мастер свяжется с вами в течение 5 минут.');
 });
